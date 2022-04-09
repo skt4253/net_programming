9a3539022bc90c561915a33d9463c4741526c9c6
Sujin Yoo <skygreedhoy@gmail.com>
2022-04-09 20:30:17 +0900

    HW8

---
+++ HW8/tcp_multi_chat_client.py
@@ -0,0 +1,25 @@
+import socket
+import threading
+
+def handler(sock):
+    while True:
+        msg = sock.recv(1024)
+        print(msg.decode())
+        
+svr_addr = ("localhost", 2500)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(svr_addr)
+
+my_id = input("ID를 입력하세요: ")
+sock.send(('['+my_id+"]").encode())
+
+th = threading.Thread(target=handler, args=(sock, ))
+th.daemon = True
+th.start()
+
+while True:
+    msg = '['+my_id+"] " + input()
+    sock.send(msg.encode())
+    if "quit" in msg:
+        break
+sock.close()
